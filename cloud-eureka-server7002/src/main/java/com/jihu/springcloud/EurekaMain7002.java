7b42a3f95f1e96ba9f685fe10a837b3ef3ffa163
huge <529581062@qq.com>
2020-04-24 20:50:23 +0800

    Eureka集群搭建

--- cloud-eureka-server7002/src/main/java/com/jihu/springcloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/jihu/springcloud/EurekaMain7002.java
@@ -2,13 +2,13 @@ package com.jihu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaClient
+@EnableEurekaServer
 @SpringBootApplication
-public class OrderMain80 {
+public class EurekaMain7002 {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(EurekaMain7002.class,args);
     }
 }
