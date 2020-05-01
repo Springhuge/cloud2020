adb7c62aebcdf000d53919f08cb0b4eb8d7d5c29
huge <529581062@qq.com>
2020-05-01 21:13:09 +0800

    注册中心consul的学习

---
+++ cloud-consumerconsul-order80/src/main/java/com/jihu/springcloud/controller/OrderController.java
@@ -0,0 +1,29 @@
+package com.jihu.springcloud.controller;
+
+import com.jihu.springcloud.entities.CommonResult;
+import com.jihu.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@RestController
+public class OrderController {
+
+    public static final String INVOKE_URL = "http://consul-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/consul")
+    public String paymentInfo(){
+        String result  = restTemplate.getForObject(INVOKE_URL+"/payment/consul",String.class);
+        return result;
+    }
+
+}
