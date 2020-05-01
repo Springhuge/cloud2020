770219353f01378891baf18996254f92cb8f82eb
huge <529581062@qq.com>
2020-05-01 21:12:25 +0800

    注册中心zookeeper的使用

---
+++ cloud-consumerzk-order80/src/main/java/com/jihu/springcloud/OrderZKMain80.java
@@ -0,0 +1,14 @@
+package com.jihu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class OrderZKMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZKMain80.class,args);
+    }
+}
